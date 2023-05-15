2160e475e5d4f8f71fb33b6ad620521769f4719e
François-Xavier Thoorens <fx.thoorens@gmail.com>
2023-05-15 23:50:40 +0200

    fixed exports

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import Challenger from "./src/Challenger.js";
 import IdManager from "./src/IdManager.js";
 import KeyManager from "./src/KeyManager.js";
 import VaultysId from "./src/VaultysId.js";
-import MemoryChannel from "./src/MemoryManager.js";
+import MemoryChannel from "./src/MemoryChannel.js";
+import MemoryStorage from "./src/MemoryStorage.js";
 import GameOfLifeIcon from "./src/GameOfLifeIcon.js";
 import CryptoChannel from "./src/cryptoChannel.js";
 
@@ -14,6 +15,7 @@ export {
   VaultysId,
   Challenger,
   MemoryChannel,
+  MemoryStorage,
   IdManager,
   KeyManager,
   GameOfLifeIcon,
