22ec2ab850cfdd4c4592630c6751f6af423813e3
François-Xavier Thoorens <fx.thoorens@gmail.com>
2023-05-16 00:19:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import Challenger from "./src/Challenger.js";
 import IdManager from "./src/IdManager.js";
 import KeyManager from "./src/KeyManager.js";
 import VaultysId from "./src/VaultysId.js";
-import MemoryChannel from "./src/MemoryChannel.js";
-import MemoryStorage from "./src/MemoryStorage.js";
+import { MemoryChannel } from "./src/MemoryChannel.js";
+import { MemoryStorage } from "./src/MemoryStorage.js";
 import GameOfLifeIcon from "./src/GameOfLifeIcon.js";
 import CryptoChannel from "./src/cryptoChannel.js";
 
